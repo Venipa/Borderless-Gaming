09b4f73e2a8786cb20a395f5cef0484a7c3f6da9
Venipa <admin@venipa.net>
2026-03-17 03:40:50 +0100

    Add Newtonsoft.Json package and enhance Tools class with update checking functionality
    - Added Newtonsoft.Json as a dependency in the project file.
    - Enhanced the Tools class with a new method to check for application updates via GitHub API.
    - Updated the release workflow to modify versioning in AssemblyInfo.cs and adjusted file paths for packaging.
    
    Signed-off-by: Venipa <admin@venipa.net>

--- BorderlessGaming.Logic/System/Tools.cs
+++ BorderlessGaming.Logic/System/Tools.cs
@@ -1,81 +1,115 @@
 ﻿using BorderlessGaming.Logic.Core;
 using BorderlessGaming.Logic.Models;
 using BorderlessGaming.Logic.System.Utilities;
+using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.IO.Compression;
 using System.Text;
+using System.Reflection;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using BorderlessGaming.Logic.Properties;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BorderlessGaming.Logic.System
 {
-    public static class Tools
+  public static class Tools
+  {
+    public static void Setup()
     {
-        public static void Setup()
-        {
-            if (!Directory.Exists(AppEnvironment.DataPath))
-            {
-                Directory.CreateDirectory(AppEnvironment.DataPath);
-            }
-            if (!Debugger.IsAttached)
-            {
-                ExceptionHandler.AddGlobalHandlers();
-            }
-            Config.Load();
-            LanguageManager.Load();
-        }
-
-        public static Rectangle GetContainingRectangle(Rectangle a, Rectangle b)
-        {
-            var amin = new Point(a.X, a.Y);
-            var amax = new Point(a.X + a.Width, a.Y + a.Height);
-            var bmin = new Point(b.X, b.Y);
-            var bmax = new Point(b.X + b.Width, b.Y + b.Height);
-            var nmin = new Point(0, 0);
-            var nmax = new Point(0, 0);
-
-            nmin.X = amin.X < bmin.X ? amin.X : bmin.X;
-            nmin.Y = amin.Y < bmin.Y ? amin.Y : bmin.Y;
-            nmax.X = amax.X > bmax.X ? amax.X : bmax.X;
-            nmax.Y = amax.Y > bmax.Y ? amax.Y : bmax.Y;
-
-            return new Rectangle(nmin, new Size(nmax.X - nmin.X, nmax.Y - nmin.Y));
-        }
-
-
-        public static void GotoSite(string url)
-        {
-            try
-            {
-                Process.Start(url);
-            }
-            catch
-            {
-                // ignored
-            }
-        }
-
-        public static void ExtractZipFile(string archiveFilenameIn, string password, string outFolder)
-        {
-            using (ZipArchive archive = ZipFile.Open(archiveFilenameIn, ZipArchiveMode.Read, Encoding.UTF8))
-            {
-                foreach (ZipArchiveEntry entry in archive.Entries)
-                {
-                    string destinationPath = Path.Combine(outFolder, entry.FullName);
-                    string destinationDirectory = Path.GetDirectoryName(destinationPath);
-
-                    if (!Directory.Exists(destinationDirectory))
-                    {
-                        Directory.CreateDirectory(destinationDirectory);
-                    }
-
-                    if (!string.IsNullOrEmpty(Path.GetFileName(destinationPath))) // Skip directories
-                    {
-                        entry.ExtractToFile(destinationPath, overwrite: true);
-                    }
-                }
-            }
-        }
-
+      if (!Directory.Exists(AppEnvironment.DataPath))
+      {
+        Directory.CreateDirectory(AppEnvironment.DataPath);
+      }
+      if (!Debugger.IsAttached)
+      {
+        ExceptionHandler.AddGlobalHandlers();
+      }
+      Config.Load();
+      LanguageManager.Load();
     }
+
+    public static Rectangle GetContainingRectangle(Rectangle a, Rectangle b)
+    {
+      var amin = new Point(a.X, a.Y);
+      var amax = new Point(a.X + a.Width, a.Y + a.Height);
+      var bmin = new Point(b.X, b.Y);
+      var bmax = new Point(b.X + b.Width, b.Y + b.Height);
+      var nmin = new Point(0, 0);
+      var nmax = new Point(0, 0);
+
+      nmin.X = amin.X < bmin.X ? amin.X : bmin.X;
+      nmin.Y = amin.Y < bmin.Y ? amin.Y : bmin.Y;
+      nmax.X = amax.X > bmax.X ? amax.X : bmax.X;
+      nmax.Y = amax.Y > bmax.Y ? amax.Y : bmax.Y;
+
+      return new Rectangle(nmin, new Size(nmax.X - nmin.X, nmax.Y - nmin.Y));
+    }
+
+
+    public static void GotoSite(string url)
+    {
+      try
+      {
+        Process.Start(url);
+      }
+      catch
+      {
+        // ignored
+      }
+    }
+
+    public static void ExtractZipFile(string archiveFilenameIn, string password, string outFolder)
+    {
+      using (ZipArchive archive = ZipFile.Open(archiveFilenameIn, ZipArchiveMode.Read, Encoding.UTF8))
+      {
+        foreach (ZipArchiveEntry entry in archive.Entries)
+        {
+          string destinationPath = Path.Combine(outFolder, entry.FullName);
+          string destinationDirectory = Path.GetDirectoryName(destinationPath);
+
+          if (!Directory.Exists(destinationDirectory))
+          {
+            Directory.CreateDirectory(destinationDirectory);
+          }
+
+          if (!string.IsNullOrEmpty(Path.GetFileName(destinationPath))) // Skip directories
+          {
+            entry.ExtractToFile(destinationPath, overwrite: true);
+          }
+        }
+      }
+    }
+
+    public static async Task CheckForUpdates()
+    {
+      try
+      {
+        var releasePageUrl = "https://github.com/Venipa/Borderless-Gaming/releases/latest";
+        const string apiUrl = "https://api.github.com/repos/Venipa/Borderless-Gaming/releases/latest";
+        var response = await new HttpClient().GetAsync(apiUrl);
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        var release = JsonConvert.DeserializeObject<dynamic>(content);
+        var latestVersion = new Version(release.tag_name.Replace("v", ""));
+        var appVersion = Assembly.GetEntryAssembly().GetName().Version;
+        if (appVersion.CompareTo(latestVersion) < 0)
+        {
+          if (MessageBox.Show(Resources.InfoUpdateAvailable, Resources.InfoUpdatesHeader,
+                  MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+          {
+            GotoSite(releasePageUrl);
+          }
+        }
+      }
+      catch
+      {
+        MessageBox.Show(Resources.ErrorUpdates, Resources.ErrorHeader, MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+    }
+
+  }
 }
